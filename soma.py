a8fa448fe86e0dd8def4e708ed90bbcf4113d34a
victorhugonicolau <victor.castro29@etec.sp.gov.br>
2024-08-22 22:28:35 -0300

    Aula-0822 - aprendendo python com o Ordine

---
+++ soma.py
@@ -0,0 +1,9 @@
+#definir variaveis
+numero1 = 5
+numero2 = 3
+
+#precessamento de dados
+resultado = numero1 + numero2
+
+#saida de dados
+print("A soma de", numero1, "e", numero2, "é", resultado, ":D")
